82e1d98db3c76a65d4abb54a96ea8fe118434e33
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2019-01-11 17:22:20 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/projectitdiv/quickmath/LoginActivity.java
+++ app/src/main/java/com/example/projectitdiv/quickmath/LoginActivity.java
@@ -1,22 +1,19 @@
 package com.example.projectitdiv.quickmath;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,8 +22,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-
-import java.lang.reflect.Member;
 
 public class LoginActivity extends AppCompatActivity {
 
